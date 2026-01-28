6ccb6d37de251f2470ffaef505af30c39750750f
jagadish <vuyyurujagadish1431@gmail.com>
2026-01-28 09:41:13 +0530

    Config files committed

---
+++ api-gateway/src/main/java/com/ordermanagement/gateway/filter/LoggingFilter.java
@@ -0,0 +1,37 @@
+package com.ordermanagement.gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+	@Component
+	public class LoggingFilter implements GlobalFilter, Ordered {
+
+	    private static final Logger log = LoggerFactory.getLogger(LoggingFilter.class);
+
+	    public LoggingFilter() {
+	        log.info("LoggingFilter loaded");
+	    }
+
+	    @Override
+	    public Mono<Void> filter(ServerWebExchange exchange,
+	                             GatewayFilterChain chain) {
+
+	        log.info("Gateway Request Path: {}",
+	                exchange.getRequest().getURI().getPath());
+
+	        return chain.filter(exchange);
+	    }
+
+	    @Override
+	    public int getOrder() {
+	        return -1;
+	    }
+	
+}
